429e4af8658bb27d569726ac2a09f0b25ce19418
NativeCode <opensource@nativecode.com>
2020-03-10 23:36:23 -0400

    fix: upgrade packages

--- packages/sonarr/specs/env.ts
+++ packages/sonarr/specs/env.ts
@@ -14,6 +14,5 @@ function endpoint(): string {
   return 'localhost'
 }
 
-export const APIKEY: string =
-  process.env.SONARR_TEST_APIKEY || process.env.SONARR_APIKEY || '31312e9d204348fda11cf569ad3d1a17'
+export const APIKEY: string = process.env.SONARR_TEST_APIKEY || process.env.SONARR_APIKEY || '31312e9d204348fda11cf569ad3d1a17'
 export const ENDPOINT = endpoint()
