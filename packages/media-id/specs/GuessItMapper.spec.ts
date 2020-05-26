0b6e49478bd0f3de235f2155e6c627b0f5792c94
NativeCode <opensource@nativecode.com>
2020-05-26 11:31:21 -0400

    fix: test

--- packages/media-id/specs/GuessItMapper.spec.ts
+++ packages/media-id/specs/GuessItMapper.spec.ts
@@ -5,7 +5,7 @@ import { GuessItMapper } from '../src/Mappers/GuessItMapper'
 
 describe('when using GuessItMapper', () => {
   it('should map movie filename', async () => {
-    const guessit = await GuessItMapper('Se7en.(1995).avi')
+    const guessit = await GuessItMapper('Se7en.(1995).avi', 'https://guessit.nativecode.com')
     expect(guessit.title).equal('Se7en')
   })
 })
